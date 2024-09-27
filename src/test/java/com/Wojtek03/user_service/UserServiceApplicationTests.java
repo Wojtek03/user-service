ac93d6750f1e7fd19cd2d360aabb3ef000942546
wojciech.biernacki <w.biernacki03@gmail.com>
2024-09-27 16:23:22 +0200

    user-service

---
+++ src/test/java/com/Wojtek03/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wojtek03.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
